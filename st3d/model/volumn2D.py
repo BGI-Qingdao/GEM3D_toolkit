1157c2e6dd021e65610c0c055c883c81937a2ac0
guolidong <guolidong@genomics.cn>
2021-09-14 22:55:08 +0800

    remove mosaic bins

--- st3d/model/volumn2D.py
+++ st3d/model/volumn2D.py
@@ -5,14 +5,24 @@ def xyv_to_volumn2D( df : pd.DataFrame, xmin, xmax, ymin,ymax,downsize):
     df.columns = ['x','y','v']
     df['x'] = df['x'] - xmin
     df['y'] = df['y'] - ymin
+    if downsize >=2 :
+        df_s = df.copy()
+        df_s['x'] = df_s['x'] + int(downsize//2)
+        df_s['y'] = df_s['y'] + int(downsize//2)
+        df_s['x'] = (df_s['x'] / downsize).astype(int)
+        df_s['y'] = (df_s['y'] / downsize).astype(int)
+
     df['x'] = (df['x'] / downsize).astype(int)
     df['y'] = (df['y'] / downsize).astype(int)
 
     df['v'] = (df['v'] +1).astype(int)
-    draw_height = int(( ymax - ymin )//downsize +1)
-    draw_width  = int(( xmax - xmin )//downsize +1)
+    draw_height = int(( ymax+downsize-1 - ymin )//downsize +1)
+    draw_width  = int(( xmax+downsize-1 - xmin )//downsize +1)
 
     coords=np.zeros((draw_height,draw_width))
     coords[df['y'], df['x']] = df['v']
+    if downsize >=2 :
+        coords[df_s['y'], df_s['x']] = df_s['v']
+
     return coords
 
