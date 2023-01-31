a96cd55aea6a3440ef916c3d44d224cb73c7bb0d
Lidong Guo <guolidong@genomics.com>
2023-01-31 15:43:46 +0800

    update

--- GEM_toolkit.py
+++ GEM_toolkit.py
@@ -10,51 +10,49 @@ import time
 # usage
 def main_usage():
     print("""
-Usage : GEM_toolkit.py action [options ]
+Usage : GEM_toolkit.py action [options]
 
-Action:
+Actions:
+
+---------------------------------------------------------------------
+
+ Workflow for single slice to generate single-cell resolved data:
+    prepare_registration_heatmap  generate 8bit bin1 heatmap with highlighted tracklines.
+    prepare_registration_ssdna    generate 8bit bin1-scaled ssDNA graph with highlighted tracklines.
+    second_registration           second round registration.
+    gem_to_gemc                   convert GEM into GEMC based on cellbin result and registration results.
+
+ Workflow for multiply slices (3D mode) to generate 3D resolved coordinates:
+    prepare_alignment_image       generate 8bit bin1 binary/annatation image for 3D alignment.
+    apply_alignment               set 3D coordinate for GEM(C)/h5ad/ssDNA/cell.mask.
+
+ Format coverting tools:
+    gem_to_h5ad                   convert GEM into h5ad by a certain binsize.
+    gemc_to_h5ad                  convert GEMC into h5ad.
+ 
+ Affine tools:
+    affine_gem                    modify the 2D coordinate in GEM(C) by user-defined affine matrix.
+    affine_h5ad                   modify the 2D coordinate in GEM(C) by user-defined affine matrix.
+    affine_ssdna                  affine the ssdna image by user-defined affine matrix.
+
+ Region of interest(ROI) tools: 
+    chopimage                     chop region of interests from whole image.
+    chopgem                       chop region of interests from GEM(C).
+
+ Mask tools:
+    mask_gem                      mask GEM(C) by mask image.
+    mask_ssdna                    mask ssDNA image by mask image.
+    mask_h5ad                     mask h5ad data by mask image.
+  
+ Visualization tools:
+    heatmap                       draw heatmap of expression counts with/without cellbin and with/without ssDNA.
+ 
+ Other tools:
+    chop_paste                    chop or paste ssDNA image. This tools is useful for ultra-large ssDNA image.
+    handle_trakEM2_matrix         covert trakEM2_matrix to standart affine matrix.
 
     -----------------------------------------------------------------
-
- actions work on GEM :
-
-    gem2bfm                 convert GEM into BFM.
-    gem_to_cfm              convert GEM into CFM
-    heatmap                 heatmap of expression counts.
-    prepareregistrationheatmap  generate 8bit bin1 heatmap with highlighted tracklines.
-    prepareregistrationssdna    generate 8bit bin1-scaled ssDNA graph with highlighted tracklines.
-
- actions work on bin5 coordinate space :
-
-    maskbfm                 mask bins by mask matrixs.
-    maskheatmap             mask heatmaps by mask matrixs.
-    apply_affinematrix      apply affinematrix to add 3D (x,y,z).
-                            coordinates into tissue-position-list.csv.
-
- actions work on affined slices :
-
-    scatter3d               intergrate affined slices into 3d.
-    model3d                 join cluster results with (x,y,z) coord.
-                            and visualize by interactive html.
-
-    segmentmodel3d          segment model3d into multiply samples.
-    segmentbfm              segment slice(s) into multiply samples.
-
-    secondregistration      second round registration
-
- actions work on intergrated 3d model:
-
-    model2mesh              generate mesh from (x,y,z) model
-    model2slices            generate aligned 2D slices from model
-
- other tools :
-    handlemasks             convert mask matrixs to (x,y) and binary graph
-    mask_xy_affine          apply affine matrixs to masks and genrate xyz
-    tightbfm                remove pure zero rows and columns.
-    chopimages              chop region of interests from whole images.
-    chopgems                chop region of interests from GEMs.
     -h/--help               show this short usage
-    -----------------------------------------------------------------
 """)
 
 # logic codes
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # GEM3D_toolkit
 
-## Overview
+NOTICE: this dev branch is still under developing, you can find previous codes in the main branch.
 
-Specially designed for Stereo-seq platform, "GEM_toolkit" is a collection of scrips that cooperates with ImageJ/TrakEM2 and CellProfiler to semi-automatic generate single-cell and 3D spatial resolved transtriptomics data from gene expression matrix (GEM).
+Specially designed for Stereo-seq platform, GEM_toolkit is a collection of scrips that cooperates with ImageJ/TrakEM2 and CellProfiler to semi-automatic generate single-cell and 3D spatial resolved transtriptomics data from gene expression matrix (GEM).
+
+GEM_toolkit also provide severial handly tools for file convert, image or data subset/mask/ROI/affine coordinate/visualize. Enjoy with GEM_toolkit.
 
 Overview of this workflow :
 
@@ -14,10 +16,61 @@ Overview of this workflow :
 * numpy
 * skimage
 * scipy
+* json
 
 ## Quick start
 
+```
+./GEM_toolkit.py -h
+
+Usage : GEM_toolkit.py action [options]
+
+Actions:
+
+---------------------------------------------------------------------
+
+ Workflow for single slice to generate single-cell resolved data:
+    prepare_registration_heatmap  generate 8bit bin1 heatmap with highlighted tracklines.
+    prepare_registration_ssdna    generate 8bit bin1-scaled ssDNA graph with highlighted tracklines.
+    second_registration           second round registration.
+    gem_to_gemc                   convert GEM into GEMC based on cellbin result and registration results.
+
+ Workflow for multiply slices (3D mode) to generate 3D resolved coordinates:
+    prepare_alignment_image       generate 8bit bin1 binary/annatation image for 3D alignment.
+    apply_alignment               set 3D coordinate for GEM(C)/h5ad/ssDNA/cell.mask.
+
+ Format coverting tools:
+    gem_to_h5ad                   convert GEM into h5ad by a certain binsize.
+    gemc_to_h5ad                  convert GEMC into h5ad.
+
+ Affine tools:
+    affine_gem                    modify the 2D coordinate in GEM(C) by user-defined affine matrix.
+    affine_h5ad                   modify the 2D coordinate in GEM(C) by user-defined affine matrix.
+    affine_ssdna                  affine the ssdna image by user-defined affine matrix.
+
+ Region of interest(ROI) tools:
+    chopimage                     chop region of interests from whole image.
+    chopgem                       chop region of interests from GEM(C).
+
+ Mask tools:
+    mask_gem                      mask GEM(C) by mask image.
+    mask_ssdna                    mask ssDNA image by mask image.
+    mask_h5ad                     mask h5ad data by mask image.
+
+ Visualization tools:
+    heatmap                       draw heatmap of expression counts with/without cellbin and with/without ssDNA.
+
+ Other tools:
+    chop_paste                    chop or paste ssDNA image. This tools is useful for ultra-large ssDNA image.
+    handle_trakEM2_matrix         covert trakEM2_matrix to standart affine matrix.
+
+    -----------------------------------------------------------------
+    -h/--help               show this short usage
+```
+
 ## Frequent Q & A
+
+TODO
 
 ## Cite us
 
