5c55f01d4c4fc1dfcb1d04aa88a3de03cd9e497b
wanxinjiang <113815624+wanxinjiang@users.noreply.github.com>
2023-02-07 14:23:55 +0800

    Add files via upload

---
+++ gemtk/affine_gem.py
@@ -0,0 +1,54 @@
+import sys
+import getopt
+import json
+import pandas as pd 
+import numpy as np
+def affine_gem_usage():
+    print("""
+Usage : affine_gem.py  -i <input.gem> \\
+                            -o <output> \\
+                            -B [backward (reverse) affine matrix] 
+                            -F [forward affine matrix]
+Example:
+        affine_gem.py  -i input.gem \\
+                            -o output.gem \\
+                            -F '[[1,0,10], [0,1,0] [0.0, 0.0, 1.0]]'    
+Notice: please provide one of [ -B , -F ], if both present, the later one will overwrite previous one.                             
+    """,flush=True) 
+
+def affine_gem_main(argv:[]):
+    inputgem=''
+    prefix=''
+    affine=''
+    try:
+        opts,args = getopt.getopt(argv,"hi:o:F:B:",["help=",
+                                                  "input=",
+                                                  "output="])
+    except getopt.GetoptError:
+        affine_gem_usage()
+        sys.exit(2)
+    
+    for opt,arg in opts:
+        if opt in ("-i", "--inputgem"):
+            inputgem=arg
+        elif opt in ("-o", "--output"):
+            prefix= arg
+        elif opt in ("-F",):
+            affine = np.matrix(np.array(json.loads(arg)))
+        elif opt in ("-B",):
+            affine = np.matrix(np.array(json.loads(arg))).I
+        elif opt in ("-h","--help"):
+            affine_gem_usage()
+            sys.exit(0)
+    if inputgem == "" or prefix == "" :
+        affine_gem_usage()
+        sys.exit(2)
+    
+    df = pd.read_csv(inputgem, sep='\t', comment='#')
+    gemxy=np.array(df[["x",'y']])
+    gemxy=np.insert(gemxy,2,values=np.ones((gemxy.shape[0],)),axis=1)
+    affine_result=np.dot(affine,gemxy.T)[0:2,:]
+    df["new_x"]=np.array(affine_result[0:1,:].T)
+    df["new_y"]=np.array(affine_result[1:2,:].T)
+    df.to_csv(prefix,index=None,sep='\t')
+
---
+++ gemtk/affine_ssdna.py
@@ -0,0 +1,85 @@
+import sys
+import getopt
+import json
+import scipy.ndimage as nd
+from skimage import io as skio
+import numpy as np
+
+# usage
+def affine_ssdna_usage():
+    print("""
+Usage : affine_ssdna.py  -i <input.tif> \\
+                             -o <output> \\
+                             -B <backword (reverse) affine matrix> \\
+                             -F <forword affine matrix> \\
+                             -r <ref image> \\
+                             -f [fliph/flipv/noflip, default noflip] 
+
+Example :
+        affine_ssdna.py -i input.tif \\
+                            -o out.tif \\
+                            -r ref.tif \\
+                            -a '[[0.023890163936836614, 1.1533635423882767, 3673.0911462805616], [-1.1509242533824746, 0.022522101202567, 22343.59128420033], [0.0, 0.0, 1.0]]' \\
+                            -f fliph
+""",flush=True)
+
+
+def affine_ssdna_main(argv:[]) :
+    inputf = ''
+    prefix = ''
+    affine=np.eye(3)
+    flip = 'noflip'
+    ref = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:F:B:f:r:",["help=" ,
+                                                     "input=",
+                                                     "output=",
+                                                     "ref=",
+                                                     "flip="
+                                                     ])
+    except getopt.GetoptError:
+        affine_ssdna_usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-i", "--inputf"):
+            inputf = arg
+        elif opt in ('-h','--help'):
+            affine_ssdna_usage()
+            sys.exit(0)
+        elif opt in ("-o", "--output"):
+            prefix = arg
+        elif opt in ('-F'):
+            affine_r = np.matrix(np.array(json.loads(arg))).I
+        elif opt in ('-B'):
+            affine_r = np.matrix(np.array(json.loads(arg)))
+        elif opt in ('-f' , '--flip'):
+            flip = arg
+        elif opt in ('-r' , '--ref'):
+            ref = arg
+
+    if inputf == '' or prefix == '' or not flip in ('fliph','flipv','noflip') or ref == '':
+        affine_ssdna_usage()
+        sys.exit(2)
+    refd = skio.imread(ref)
+    w,h = refd.shape
+    print(f'ref w={w}, h={h}')
+
+    dapi_data  = skio.imread(inputf)
+    if len(dapi_data.shape) == 3 : # RGB tiff to 8 bit gray tiff
+        new_data = np.zeros((dapi_data.shape[0],dapi_data.shape[1]),dtype=int)
+        new_data = new_data + dapi_data[:,:,0]
+        new_data = new_data + dapi_data[:,:,1]
+        new_data = new_data + dapi_data[:,:,2]
+        new_data = (new_data+2) / 3
+        dapi_data = new_data
+    dapi_data = dapi_data.astype('uint8')
+    ind = dapi_data
+    if flip == 'fliph':
+        ind = np.fliplr(ind)
+    elif flip == 'flipv':
+        ind = np.flipud(ind)
+    outd = nd.affine_transform(ind.T,affine_r,output_shape=(h,w),order=0)
+    outd = outd.T
+    outd = outd.astype('uint8')
+    print(outd.shape)
+    skio.imsave(prefix,outd)
